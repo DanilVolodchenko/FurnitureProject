d039b84caeb1e46aa1ecce0c0e6d09a1ce2462e9
Danil Volodchenko <danil.volodchenko@inbox.ru>
2024-11-03 18:57:09 +0700

    [*] modifing furniture router and security in common

--- src/furniture/__init__.py
+++ src/furniture/__init__.py
@@ -1,1 +1,5 @@
 from .routers import router as back_router
+
+__all__ = [
+    back_router
+]
--- src/furniture/routers.py
+++ src/furniture/routers.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import Session
 from src.database import session
 from . import schemas, typing
 from .managers import Manager
-from .services import FurnitureService, ImageService
+from .services import FurnitureService, ImageService, CategoryService
 from .validators import validate_media_type
-from src.common.utils import check_token
+from ..common.security import check_token
 
 router = APIRouter(prefix='/api/v1', tags=['Furniture'])
 
@@ -39,29 +39,32 @@ async def get_image(furniture_id: int, image_name: typing.UUID, session: Annotat
 
 @router.post('/furniture', dependencies=[Depends(check_token)])
 async def create_furniture(
-        request: Request,
-        furniture_data: Annotated[schemas.CreateFurniture, Depends()],
+        created_furniture: Annotated[schemas.CreateFurniture, Depends()],
         files: Annotated[list[UploadFile], Depends(validate_media_type)],
         session: Annotated[Session, Depends(session)]
 ):
     """Создание мебельного продукта."""
 
-    return Manager(session).create(furniture=furniture_data, files=files)
+    return Manager(
+        CategoryService(session), FurnitureService(session), ImageService(session)
+    ).create(created_furniture=created_furniture, files=files)
 
 
 @router.put('/furniture/{furniture_id}')
 async def update_furniture(
         furniture_id: Annotated[int, Path()],
-        furniture_data: Annotated[schemas.UpdateFurniture, Depends()],
+        updated_furniture: Annotated[schemas.UpdateFurniture, Depends()],
         files: Annotated[list[UploadFile], Depends(validate_media_type)],
         session: Annotated[Session, Depends(session)]
 ):
     """Обновление мебельного продукта."""
 
-    return Manager(session).update(furniture=furniture_data, files=files)
+    return Manager(
+        CategoryService(session), FurnitureService(session), ImageService(session)
+    ).update(furniture_id=furniture_id, updated_furniture=updated_furniture, files=files)
 
 
-@router.put('/category/{category_id}')
+@router.put('/category/{category_id}')  # TODO доделать роутер
 async def update_category(
         category_id: int, category_s: schemas.Furniture,
         session: Annotated[Session, Depends(session)]
